101375a76a7b1d6059750e94cfc260bb3d3e437a
merveozer <ozeermerve@gmail.com>
2024-09-23 16:07:32 +0300

    Microservice [Initial commit]

---
+++ order-service/src/main/java/com/ozer/microservices/order_service/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.ozer.microservices.order_service.service;
+
+import com.ozer.microservices.order_service.client.InventoryClient;
+import com.ozer.microservices.order_service.dto.OrderRequest;
+import com.ozer.microservices.order_service.model.Order;
+import com.ozer.microservices.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
+    public void placeOrder(OrderRequest orderRequest){
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+    }
+}
---
+++ product-service/src/test/java/com/ozer/microservices/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,56 @@
+package com.ozer.microservices.product_service;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductServiceApplicationTests {
+
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateProduct() {
+		String requestBody = """
+				{
+				        "name": "iPhone 16",
+				        "description": "smartphone",
+				        "price": 1000
+				    }
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 16"))
+				.body("description", Matchers.equalTo("smartphone"))
+				.body("price", Matchers.equalTo(1000));
+    }
+
+}
